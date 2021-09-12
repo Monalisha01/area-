458ba481b38baa77e80d95f36be9e41a5090c73e
Monalisha01 <90247327+Monalisha01@users.noreply.github.com>
2021-09-12 12:38:28 +0530

    Add files via upload

---
+++ extension2.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 12 12:35:51 2021
+
+@author: Asus
+"""
+
+# extension of a file
+
+filename= input ("input the filename: ")
+f_ext= filename.split(".")
+print ("The extension of the file is: "+ repr(f_ext[-1]))
