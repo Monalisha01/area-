63535425e7dd4ad5dae0975b6fcf24f8dab10f73
Monalisha01 <90247327+Monalisha01@users.noreply.github.com>
2021-09-12 12:22:04 +0530

    Add files via upload

---
+++ area1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 16:47:48 2021
+
+@author: Asus
+"""
+
+# area of a circle
+
+r = float(input("Input the radius of the circle: "))
+pi= 3.14
+A= pi*r*r
+print ("The area of the circle with radius",r,"is: ",A)
