2d2ccee178fd39254bc1bbf7ee21cb59c6055ede
Monalisha01 <90247327+Monalisha01@users.noreply.github.com>
2021-09-17 19:20:28 +0530

    Add files via upload

---
+++ listmycap.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 23:44:42 2021
+
+@author: Asus
+"""
+
+list1=[]
+n=int(input("Enter number of elements:"))
+for i in range(0,n):
+    e= int(input())
+    list1.append(e)
+print ("Input: ", end=" ")
+print (list1)
+li=list1
+p=0
+
+for p in li:
+    if p>0:
+         print(p)
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+        
+         
+           
+   
+    
+            
+    
+        
